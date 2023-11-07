29f72b7025b2099cb3c6ca6409de104e463a257a
charles-ide <charles.adams.ide@gmail.com>
2023-11-07 08:11:26 -0500

    Working prototype with hard coded cities

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # flask-tutorial
-This repository will be used to store code for my first Flask tutorial to learn the basics
+This repository will be used to store code for my first Flask tutorial to learn the basics. 
+The app will display the weather for any city that a user inputs.
+
+
--- app.py
+++ app.py
@@ -4,13 +4,12 @@ This application will create a webpage and display the weather in a sepcified ci
 '''
 import requests
 from flask import Flask, render_template, request
+
 from flask_sqlalchemy import SQLAlchemy 
-#from create_db import create_table
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-#create_table()
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///weather.db'
 
 db = SQLAlchemy(app)
@@ -30,7 +29,8 @@ def index():
             db.session.add(new_city_obj)
             db.session.commit()
 
-    cities = City.query.all()
+    #cities = City.query.all()
+    cities = ['Boston', 'Las Vegas'] 
 
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=b91b86148c8617a364e0f4e6ec2ca5b3'
 
@@ -38,11 +38,12 @@ def index():
 
     for city in cities:
 
-        r = requests.get(url.format(city.name)).json()
+        r = requests.get(url.format(city)).json()
 
         weather = {
-            'city' : city.name,
-            'temperature' : r['main']['temp'],
+            'city' : city,
+            'temperature_f' : int(r['main']['temp']),
+            'temperature_c' : int((r['main']['temp'] - 32) / 1.8),
             'description' : r['weather'][0]['description'],
             'icon' : r['weather'][0]['icon'],
         }
@@ -51,3 +52,6 @@ def index():
 
 
     return render_template('weather.html', weather_data=weather_data)
+
+if __name__ =='__main__':
+    app.run(debug=True)
